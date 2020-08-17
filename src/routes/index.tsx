4c741e14774cf8ac1b963abe74d5935595cf4170
Albert-dias <albert.uft@gmail.com>
2020-08-17 20:56:47 -0300

    created router home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,13 +5,16 @@ import Development from '../pages/Development';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import About from '../pages/About';
+import Home from '../pages/Home';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Development}/>
     <Route path="/login" component={SignIn}/>
     <Route path="/cadastro" component={SignUp}/>
-    <Route path="/sobre" component={About}/>  </Switch>
+    <Route path="/sobre" component={About}/>
+    <Route path="/home" component={Home}/>
+  </Switch>
 );
 
 export default Routes;
