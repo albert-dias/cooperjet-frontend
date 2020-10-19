8d88060251ffd518b9ba9bc5a410d140b7c16f35
Albert Dias <albert.uft@gmail.com>
2020-10-18 23:26:02 -0300

    update contact

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -36,19 +36,19 @@ const About: React.FC = () => {
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3968.751626075929!2d-35.19770378590387!3d-5.890478095732441!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7b2f8bf29b8980f%3A0x19912d09de2ff6f5!2sR.%20Peroba%2C%2055%20-%20Nova%20Parnamirim%2C%20Parnamirim%20-%20RN%2C%2059152-050!5e0!3m2!1spt-BR!2sbr!4v1600398061205!5m2!1spt-BR!2sbr"
               width="100%"
               height="200px"
-              title="Endereço base">
+              title="Endereço Matriz">
             </iframe>
             <p><strong>Matriz: </strong>R. Peroba, 55A, Nova Parnamirim, Natal - RN </p>
-            <p><strong>Telefone: </strong>(84)987654321 / 3214-1234</p>
+            <p><strong>Telefone: </strong>(84) 98880-6878 / 2010-6394</p>
           </LeftBox>
           <LeftBox>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3968.751626075929!2d-35.19770378590387!3d-5.890478095732441!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7b2f8bf29b8980f%3A0x19912d09de2ff6f5!2sR.%20Peroba%2C%2055%20-%20Nova%20Parnamirim%2C%20Parnamirim%20-%20RN%2C%2059152-050!5e0!3m2!1spt-BR!2sbr!4v1600398061205!5m2!1spt-BR!2sbr"
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3968.484972609231!2d-35.261383885935025!3d-5.927673559962874!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7b2577582317ba7%3A0xd99926b9426c710d!2sR.%20Gov.%20Juvenal%20Lamartine%2C%20566c%20-%20Boa%20Esperan%C3%A7a%2C%20Parnamirim%20-%20RN%2C%2059140-360!5e0!3m2!1spt-BR!2sbr!4v1603074193289!5m2!1spt-BR!2sbr"
               width="100%"
               height="200px"
-              title="Endereço base">
+              title="Endereço filial">
             </iframe>
-            <p><strong>Matriz: </strong>R. Peroba, 55A, Nova Parnamirim, Natal - RN </p>
-            <p><strong>Telefone: </strong>(84)987654321 / 3214-1234</p>
+            <p><strong>Filial: </strong>R. Governador Juvenal Lamartine, 566b - Boa Esperança, Zona Leste, Parnamirim - RN</p>
+            <p><strong>Telefone: </strong>(84) 3272-1763</p>
           </LeftBox>
         </Limitation>
       </Container>
